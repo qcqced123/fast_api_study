047ee984d0c37c307bbfefb6d0340fb6d001bd03
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2023-03-01 22:53:02 +0900

    03.01 Study

--- myapi/frontend/src/App.svelte
+++ myapi/frontend/src/App.svelte
@@ -1,11 +1,10 @@
 <script>
-    let message;
-    
-    fetch("http://127.0.0.1:8000/hello").then((response) => {
-        response.json().then((json) => {
-            message = json.message;
-        });
-    });
-</script>
-
-<h1>{message}</h1>
+    import Router from 'svelte-spa-router'
+    import Home from "./routes/Home.svelte"
+  
+    const routes = {
+      '/': Home,
+    }
+  </script>
+  
+  <Router {routes}/>
---
+++ myapi/frontend/src/routes/Home.svelte
@@ -0,0 +1,18 @@
+<script>
+    import fastapi from "../lib/api"
+    let question_list = []
+  
+    function get_question_list() {
+      fastapi('get', '/api/question/list', {}, (json) => { //success_callback
+        question_list = json
+      }) // not init failure_callback parameter: fastapi automatically alert & print error
+    }
+  
+    get_question_list()
+  </script>
+  
+  <ul>
+    {#each question_list as question}
+      <li>{question.subject}</li>
+    {/each}
+  </ul>
